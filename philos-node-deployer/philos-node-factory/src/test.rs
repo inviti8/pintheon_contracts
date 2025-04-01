9113c8aff4b53fb79923b52bca6fdd4a66cfe6c5
Jason <dimensions3@outlook.com>
2025-03-31 20:54:17 -0700

    Added node contracts

---
+++ philos-node-deployer/philos-node-factory/src/test.rs
@@ -0,0 +1,61 @@
+#![cfg(test)]
+extern crate alloc;
+extern crate std;
+
+use crate::{PhilosFactory, PhilosFactoryClient};
+use alloc::vec;
+use soroban_sdk::{
+    symbol_short,
+    testutils::{Address as _, AuthorizedFunction, AuthorizedInvocation},
+    Address, BytesN, Val, Env, FromVal, IntoVal, String, Vec,
+};
+
+// The contract that will be deployed by the deployer contract.
+mod contract {
+    soroban_sdk::contractimport!(
+        file =
+            "../philos-node-token/target/wasm32-unknown-unknown/release/philos_node_token.wasm"
+    );
+}
+
+#[test]
+fn test() {
+    let env = Env::default();
+    let admin = Address::generate(&env);
+    let deployer_client = PhilosFactoryClient::new(&env, &env.register(PhilosFactory, (&admin,)));
+    let name = String::from_val(&env, &"name");
+    let symbol = String::from_val(&env, &"symbol");
+    let node_id = String::from_val(&env, &"NODE_ID");
+    let descriptor = String::from_val(&env, &"DESCRIPTOR");
+    let established = String::from_val(&env, &"ESTABLISHED");
+
+    // Upload the Wasm to be deployed from the deployer contract.
+    // This can also be called from within a contract if needed.
+    let wasm_hash = env.deployer().upload_contract_wasm(contract::WASM);
+
+    // Deploy contract using deployer, and include an init function to call.
+    let salt = BytesN::from_array(&env, &[0; 32]);
+    let constructor_args: Vec<Val> = (admin.clone(), 5u32, name.clone(), symbol.clone(), node_id.clone(), descriptor.clone(), established.clone()).into_val(&env);
+    env.mock_all_auths();
+    let contract_id = deployer_client.deploy(&wasm_hash, &salt, &constructor_args);
+
+    // An authorization from the admin is required.
+    let expected_auth = AuthorizedInvocation {
+        // Top-level authorized function is `deploy` with all the arguments.
+        function: AuthorizedFunction::Contract((
+            deployer_client.address,
+            symbol_short!("deploy"),
+            (wasm_hash.clone(), salt, constructor_args).into_val(&env),
+        )),
+        sub_invocations: vec![],
+    };
+    assert_eq!(env.auths(), vec![(admin, expected_auth)]);
+
+    // Invoke contract to check that it is initialized.
+    let token = contract::Client::new(&env, &contract_id);
+    assert_eq!(token.name(), name);
+    assert_eq!(token.symbol(), symbol);
+    assert_eq!(token.node_id(), node_id);
+    assert_eq!(token.descriptor(), descriptor);
+    assert_eq!(token.established(), established);
+}
---
+++ philos-node-deployer/philos-node-token/src/metadata.rs
@@ -0,0 +1,43 @@
+use soroban_sdk::{Env, String};
+use hvym_node_token::{nodemetadata::NodeTokenMetadata, TokenUtils};
+
+pub trait NodeTokenInterface {
+    fn node_id(env: Env) -> String;
+    fn descriptor(env: Env) -> String;
+    fn established(env: Env) -> String;
+}
+
+pub fn read_decimal(e: &Env) -> u32 {
+    let util = TokenUtils::new(e);
+    util.metadata().get_metadata().decimal
+}
+
+pub fn read_name(e: &Env) -> String {
+    let util = TokenUtils::new(e);
+    util.metadata().get_metadata().name
+}
+
+pub fn read_symbol(e: &Env) -> String {
+    let util = TokenUtils::new(e);
+    util.metadata().get_metadata().symbol
+}
+
+pub fn read_node_id(e: &Env) -> String {
+    let util = TokenUtils::new(e);
+    util.metadata().get_metadata().node_id
+}
+
+pub fn read_descriptor(e: &Env) -> String {
+    let util = TokenUtils::new(e);
+    util.metadata().get_metadata().descriptor
+}
+
+pub fn read_established(e: &Env) -> String {
+    let util = TokenUtils::new(e);
+    util.metadata().get_metadata().established
+}
+
+pub fn write_metadata(e: &Env, metadata: NodeTokenMetadata) {
+    let util = TokenUtils::new(e);
+    util.metadata().set_metadata(&metadata);
+}
