17d6b5e90afd2d3e19342db0cc761aed3ef3191f
metavinci <pszdw-75nat-5227i-bha5s-y7lai-pebdq-o2agp-3xho4-hd6z6-emxrd-nqe@dmail.ai>
2025-04-11 17:42:39 -0700

    simplify

--- hvym-collective-factory/src/lib.rs
+++ hvym-collective-factory/src/lib.rs
@@ -37,33 +37,19 @@ impl CollectiveFactory {
         let salt = BytesN::from_array(&env, &ran);
         let constructor_args: Vec<Val> = (admin.clone(), &JOIN_FEE,  &MINT_FEE, xlm.clone(), &OPUS_REWARD).into_val(&env);
 
-        let contract_id = Self::deploy(env.clone(), wasm_hash.clone(), salt.clone(), constructor_args.clone());
-
-        contract_id
-    }
-
-    /// Deploys the contract on behalf of the `CollectiveFactory` contract.
-    ///
-    /// This has to be authorized by the `CollectiveFactory`s administrator.    
-    fn deploy(
-        env: Env,
-        wasm_hash: BytesN<32>,
-        salt: BytesN<32>,
-        constructor_args: Vec<Val>,
-    ) -> Address {
-
         let deployed_address = env
             .deployer()
             .with_address(env.current_contract_address(), salt)
             .deploy_v2(wasm_hash, constructor_args);
 
         deployed_address
+
     }
 
     fn native_asset_contract_address(e: &Env, mainnet: bool) -> Address {
         let mut str_addr = String::from_str(&e, &"CDLZFC3SYJYDZT7K67VZ75HPJVIEUVNIXF47ZG2FB2RMQQVU2HHGCYSC");
         if mainnet {
-            str_addr = String::from_str(&e, &"CDLZFC3SYJYDZT7K67VZ75HPJVIEUVNIXF47ZG2FB2RMQQVU2HHGCYSC");
+            str_addr = String::from_str(&e, &"CAS3J7GYLGXMF6TDJBBYYSE3HQ6BBSMLNUQ34T6TZMYMW2EVH34XOWMA");
         }
         
         let native_asset_address = Address::from_string(&str_addr);
